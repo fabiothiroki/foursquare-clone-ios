5090f492261f80074852144fe4729c033be1160a
Fabio Hiroki <fabiothiroki@gmail.com>
2017-10-08 20:25:03 -0300

    Added cocoapods dependencies

---
+++ Pods/Target Support Files/Pods-Foursquare Clone/Pods-Foursquare Clone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Foursquare_Clone : NSObject
+@end
+@implementation PodsDummy_Pods_Foursquare_Clone
+@end
---
+++ Pods/Target Support Files/Pods-Foursquare CloneTests/Pods-Foursquare CloneTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Foursquare_CloneTests : NSObject
+@end
+@implementation PodsDummy_Pods_Foursquare_CloneTests
+@end
