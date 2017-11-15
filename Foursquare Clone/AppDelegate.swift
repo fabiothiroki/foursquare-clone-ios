240af0c5b2355206b5ad501e3db7eb4d1dd1065c
Fabio Hiroki <fabiothiroki@gmail.com>
2017-11-15 21:38:07 -0200

    Basic store configuration

--- Foursquare Clone/AppDelegate.swift
+++ Foursquare Clone/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import ReSwift
 
+let store = Store<FetchedPlacesState>(reducer: appReducer, state: nil)
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -20,25 +22,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
