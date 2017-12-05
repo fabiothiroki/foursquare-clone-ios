94bc671ab4bf838269c400ca8cac41bf3b21b5d2
Fabio Hiroki <fhiroki@nic.br>
2017-12-05 18:57:16 -0200

    Test resolve reducer dependencies

---
+++ Reducers/AppReducerSpec.swift
@@ -0,0 +1,47 @@
+//
+//  AppReducerSpec.swift
+//  Foursquare CloneTests
+//
+//  Created by Fabio Hiroki on 05/12/17.
+//  Copyright © 2017 Fabio Hiroki. All rights reserved.
+//
+
+import XCTest
+import Swinject
+import ReSwift
+
+class AppReducerSpec: XCTestCase {
+
+    private var container: Container!
+    private var reducer: AppReducer!
+
+    override func setUp() {
+        super.setUp()
+        
+        container = Container()
+        container.register(UserLocationDatasource.self) { _ in UserLocationServiceMock() }
+            .inObjectScope(.container)
+        container.register(PlacesDatasource.self) { _ in PlacesApiMock()}
+            .inObjectScope(.container)
+        container.register(NearbyPlacesService.self) { resolver in
+            NearbyPlacesService.init(userLocationDatasource: resolver.resolve(UserLocationDatasource.self)!,
+                                     placesDatasource: resolver.resolve(PlacesDatasource.self)!)}
+
+        container.register(AppReducer.self) { resolver in
+            AppReducer.init(resolver.resolve(NearbyPlacesService.self)!)
+            }.initCompleted { (resolver, appReducer) in
+                var reducer = appReducer
+                reducer.store = resolver.resolve(Store<FetchedPlacesState>.self)
+        }
+
+        container.register(Store<FetchedPlacesState>.self) { resolver in
+            Store<FetchedPlacesState>(reducer: (resolver.resolve(AppReducer.self)!).reduce, state: nil)
+            }.inObjectScope(.container)
+
+        reducer = container.resolve(AppReducer.self)
+    }
+
+    func testShouldResolveDependencies() {
+        XCTAssertNotNil(reducer)
+    }
+}
