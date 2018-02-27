9ef79c9b2b34f14b94b6c995de31f7ec6a85c5fb
Fabio Hiroki <fhiroki@nic.br>
2018-02-27 19:59:28 -0300

    Better readability

--- Foursquare Clone/Extensions/UIView+FoursquareClone.swift
+++ Foursquare Clone/Extensions/UIView+FoursquareClone.swift
@@ -13,7 +13,7 @@ extension UIView {
         get {
             return layer.cornerRadius
         }
-        
+
         set {
             layer.cornerRadius = newValue
         }
@@ -22,7 +22,7 @@ extension UIView {
         get {
             return layer.shadowRadius
         }
-        
+
         set {
             layer.shadowRadius = newValue
         }
@@ -31,36 +31,59 @@ extension UIView {
         get {
             return layer.shadowOpacity
         }
-        
+
         set {
             layer.shadowOpacity = newValue
         }
     }
+
     @IBInspectable public var shadowColor: UIColor? {
         get {
             return layer.shadowColor != nil ? UIColor(cgColor: layer.shadowColor!) : nil
         }
-        
+
         set {
             layer.shadowColor = newValue?.cgColor
         }
     }
+
     @IBInspectable public var shadowOffset: CGSize {
         get {
             return layer.shadowOffset
         }
-        
+
         set {
             layer.shadowOffset = newValue
         }
     }
+
     @IBInspectable public var zPosition: CGFloat {
         get {
             return layer.zPosition
         }
-        
+
         set {
             layer.zPosition = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor != nil ? UIColor(cgColor: layer.borderColor!) : nil
+        }
+
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+
+        set {
+            layer.borderWidth = newValue
         }
     }
 }
